bc76464eba21d464166d498ef3b0ff61a5446119
jegyun-Park <parkjekyun@gmail.com>
2021-02-16 13:46:21 +0900

    React Movie Application ver  0.3 : text-decoration will not appear, i fixed it :)

---
+++ src/routes/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div className="about__container">
+      <span>This is About Page</span>
+    </div>
+  );
+}
+
+export default About;
